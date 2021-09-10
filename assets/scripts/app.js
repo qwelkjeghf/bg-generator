aaf22c4021852d6041fd06831afc02c12914b55a
qwelkjeghf <tehbondz@gmail.com>
2021-09-10 12:01:13 -0400

    Update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,7 @@
 // 				2. Merge local assets into clone folder, manually.
 //				3. Change terminal directory to clone folder: cd DIR
 //				4. View untracked files/folders: git status
-//				5. Add each untracked file/folder: git add filename.fileextension
+//				5. Add each untracked file/folder: git add filename.fileextension OR to add everything git add .
 //				6. Run git status again to confirm no further untracked files
 //				7. Commit changes in preparation for push, add message describing changes: git commit -m MSG
 //				8. Push files to ghub: git push

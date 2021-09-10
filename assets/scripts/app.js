de6e25735fee6265e8843e2989ce0a36ba40554c
qwelkjeghf <tehbondz@gmail.com>
2021-09-10 11:58:45 -0400

    Git Process Comments Added.

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+// Sequence for posting/retrieval is:
+
+//				1. Clone repo from ghub: git clone URL
+// 				2. Merge local assets into clone folder, manually.
+//				3. Change terminal directory to clone folder: cd DIR
+//				4. View untracked files/folders: git status
+//				5. Add each untracked file/folder: git add filename.fileextension
+//				6. Run git status again to confirm no further untracked files
+//				7. Commit changes in preparation for push, add message describing changes: git commit -m MSG
+//				8. Push files to ghub: git push
+
+//

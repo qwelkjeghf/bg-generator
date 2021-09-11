3e004486fbcd469cc11ff70d7b451c9545df046f
qwelkjeghf <tehbondz@gmail.com>
2021-09-11 08:00:20 -0400

    Updates

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,3 +15,12 @@
 
 //									1. Ensure editor is in the proper directory(repo retrieved from ghub)
 //									2. Once the above steps have been completed and before step 7 run: git pull
+
+//
+
+// Sequence for branching is:
+
+//							1. To view all branches: git branch
+//							2. To create new branch: git branch BRANCHNAME
+//							3. To switch to target branch: git checkout TARGETBRANCH
+//							4. Merging branches done on ghub
